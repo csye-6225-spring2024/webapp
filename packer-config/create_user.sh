24a1de3d2ac3c583e40fb6999e359b9bf726708a
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-02-20 09:53:57 -0500

    Added changes

---
+++ packer-config/create_user.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# sudo groupadd csye6225
+# sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
+# sudo chown -R csye6225:csye6225 -R /opt/webapp/
+# sudo chmod -R 755 /opt/webapp
+
+# sudo chmod +x server.js
+
+
+
+# cd /opt 
+# ls -l
+
+# cd /opt/webapp/
+# sudo npm install
+
