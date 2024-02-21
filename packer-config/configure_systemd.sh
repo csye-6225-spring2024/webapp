a289410df8d772e22250ac183bd6fc1d8f00efa5
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-02-21 17:30:15 -0500

    Added integration testing code

--- packer-config/configure_systemd.sh
+++ packer-config/configure_systemd.sh
@@ -7,8 +7,10 @@ sudo chmod 750 /etc/systemd/system/webapp.service
 sudo chown -R csye6225:csye6225 /opt/csye6225/
 sudo chmod -R 750 /opt/csye6225/webapp
 
-sudo systemctl daemon-reload
+
 sudo systemctl enable webapp
 sudo systemctl start webapp
-sudo systemctl restart webapp
-sudo systemctl stop webapp
+sudo systemctl status webapp
+sudo systemctl daemon-reload
+#sudo systemctl restart webapp
+# sudo systemctl stop webapp
