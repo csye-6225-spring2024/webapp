ebcbae9e33a7806fb5c1b03c28fda303110fe18c
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-02-21 14:21:02 -0500

    Changed the integration flow

--- packer-config/create_user.sh
+++ packer-config/create_user.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 
-# sudo unzip webapp.zip -d /opt/webapp
-# sudo groupadd csye6225
-# sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
-
-# sudo chown -R csye6225:csye6225 /opt/webapp
-# sudo chmod -R 755 /opt/webapp
-
-# cd /opt/webapp/
-# sudo npm install
-
-
 sudo unzip webapp.zip -d /opt/webapp
-cd /opt/webapp/
-sudo npm install
 sudo groupadd csye6225
 sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
+
 sudo chown -R csye6225:csye6225 /opt/webapp
-sudo chmod -R 750 /opt/webapp
+sudo chmod -R 755 /opt/webapp
+
+cd /opt/webapp/
+sudo npm install
+
+
+# sudo unzip webapp.zip -d /opt/webapp
+# cd /opt/webapp/
+# sudo npm install
+# sudo groupadd csye6225
+# sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
+# sudo chown -R csye6225:csye6225 /opt/webapp
+# sudo chmod -R 750 /opt/webapp
