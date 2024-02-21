502e617da9c1d8dfc69af99a6f14384f8dfb07c4
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-02-21 02:14:01 -0500

    Added changes

--- packer-config/install_dependencies.sh
+++ packer-config/install_dependencies.sh
@@ -21,7 +21,7 @@ sudo groupadd csye6225
 sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
 
 sudo chown -R csye6225:csye6225 /opt/webapp
-sudo chmod -R 750 /opt/webapp
+sudo chmod -R 755 /opt/webapp
 
 cd /opt/webapp/
 sudo npm install
