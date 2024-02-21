e9c34a32ec8c4fa240744cb1adfe70d1c5fa0b99
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-02-21 02:02:03 -0500

    Added changes

--- packer-config/install_dependencies.sh
+++ packer-config/install_dependencies.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #Install MySQL
 sudo yum install -y mysql-server
 sudo systemctl start mysqld
@@ -11,21 +10,18 @@ sudo mysql -u root -e "CREATE DATABASE db;"
 sudo mysql -u root -e "CREATE USER 'clouduser'@'localhost' IDENTIFIED BY 'dbpass';"
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON db.* TO 'clouduser'@'localhost';"
 
-
 #Install node dependencies
 sudo dnf module list nodejs
 sudo dnf module enable -y nodejs:20
 sudo dnf install -y npm
 sudo yum install zip unzip -y
 
-
 sudo unzip webapp.zip -d /opt/webapp
 sudo groupadd csye6225
 sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
 
 sudo chown -R csye6225:csye6225 /opt/webapp
-sudo chmod -R 755 /opt/webapp
-
+sudo chmod -R 750 /opt/webapp
 
 cd /opt/webapp/
 sudo npm install
