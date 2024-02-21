84bc57bf2430e15283d3cb483952b36120295726
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-02-21 02:35:22 -0500

    Added changes

--- packer-config/create_user.sh
+++ packer-config/create_user.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 
-# sudo groupadd csye6225
-# sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
-# sudo chown -R csye6225:csye6225 -R /opt/webapp/
-# sudo chmod -R 755 /opt/webapp
-
-# sudo chmod +x server.js
-
-
-
-# cd /opt 
-# ls -l
-
-# cd /opt/webapp/
-# sudo npm install
+sudo unzip webapp.zip -d /opt/webapp
+sudo groupadd csye6225
+sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
+ 
+sudo chown -R csye6225:csye6225 /opt/webapp
+# sudo chmod -R 750 /opt/webapp
+ 
+cd /opt/webapp/
+sudo npm install
 
