c9e1c861c14c3a37932583bbfab4d1c8ee69c6da
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-03-17 17:15:58 -0400

    Added code

--- logger.js
+++ logger.js
@@ -6,7 +6,7 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: '/var/log/webapp/webapp.log' })
+    new winston.transports.File({ filename: 'app.log' })
   ]
 });
 
@@ -15,63 +15,5 @@ logger.info('This is an information message');
 logger.error('This is an error message');
 logger.warn('This is a warning message');
 
-
-// const { createLogger, format, transports } = require('winston');
-// const { LoggingWinston } = require('@google-cloud/logging-winston');
-
-// // Create a Winston logger instance
-// const logger = createLogger({
-//   format: format.combine(
-//     format.timestamp(),
-//     format.json() 
-//   ),
-//   transports: [
-//     new transports.Console(), 
-//     new LoggingWinston({ 
-//       projectId: 'YOUR_PROJECT_ID',
-//     }),
-//   ],
-// });
-
-// // Example usage
-// logger.info('Application started', { version: '1.0.0', environment: 'production' });
-// logger.error('An error occurred', { error: 'Some error message', code: 500 });
-
-
-// ----------------------------
-
-// const winston = require('winston');
-// const path = require('path');
-
-// const logDirectory = process.env.NODE_ENV === 'test' ? '.' : '/var/log/webapp/';
-
-// // Define the custom Winston logger
-// const logger = winston.createLogger({
-//   // Set up the logger format
-//   format: winston.format.combine(
-//     winston.format.timestamp({
-//       format: 'YYYY-MM-DD HH:mm:ss'
-//     }),
-//     winston.format.printf(info => ${info.timestamp} ${info.level}: ${info.message})
-//   ),
-//   // Define the transports (where to log the messages to)
-//   transports: [
-//     // Define a file transport for all logs
-//     new winston.transports.File({
-//       filename: path.join(logDirectory, 'csye6225.log'),
-//       level: 'debug',
-//     }),
-//   ],
-//   exitOnError: false,
-// });
-
-// logger.stream = {
-//     write: function (message) {
-//       // Use the 'info' log level so the output will be picked up by both transports (console and file)
-//       logger.info(message.trim());
-//     },
-// };
-
-// // Export the logger so it can be used in other files
 export default logger;
 
