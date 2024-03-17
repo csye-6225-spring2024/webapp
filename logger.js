e82db18a75c52a8b00336e02e51cac01eb838607
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-03-17 17:26:03 -0400

    Added code

--- logger.js
+++ logger.js
@@ -6,7 +6,7 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'app.log' })
+    new winston.transports.File({ filename: '/var/log/webapp.log' })
   ]
 });
 
