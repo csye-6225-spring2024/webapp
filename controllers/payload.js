e1a5d43540a88e49876ac0d458bc4b2ef944f856
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-03-19 19:02:00 -0400

    Added ops agent

--- controllers/payload.js
+++ controllers/payload.js
@@ -2,6 +2,7 @@ const handlePayloadChecks = (req, res, next) => {
 
     // Check if 'content-length' header is present and greater than 0
     if (req.headers['content-length'] && req.headers['content-length'] > 0) {
+        logger.warn('Content length header present and greater than 0.');
         return res
             .status(400)
             .set("Cache-Control", "no-cache, no-store, must-revalidate")
@@ -12,6 +13,7 @@ const handlePayloadChecks = (req, res, next) => {
 
     // Check if req.body is defined and non-empty
     if (req.body && Object.keys(req.body).length > 0) {
+        logger.warn('Request body is defined and non-empty.');
         return res
             .status(400)
             .set("Cache-Control", "no-cache, no-store, must-revalidate")
@@ -22,6 +24,7 @@ const handlePayloadChecks = (req, res, next) => {
 
     // Check if query parameters are present
     if (req.query && Object.keys(req.query).length > 0) {
+        logger.warn('Query parameters are present.');
         return res
             .status(400)
             .set("Cache-Control", "no-cache, no-store, must-revalidate")
