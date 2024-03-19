68fdd69748165b7eff6aaf283753b3c4af42c4fe
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-03-19 13:43:04 -0400

    Added OPS Agent

--- logger.js
+++ logger.js
@@ -1,19 +1,38 @@
-import winston from 'winston';
+// import winston from 'winston';
 
-// Create a logger instance
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: '/var/log/webapp/myapp.log' })
+// // Create a logger instance
+// const logger = winston.createLogger({
+//   level: 'info',
+//   format: winston.format.json(),
+//   transports: [
+//     new winston.transports.Console(),
+//     new winston.transports.File({ filename: '/var/log/webapp/myapp.log' })
+//   ]
+// });
+
+// // Log messages
+// logger.info('This is an information message');
+// logger.error('This is an error message');
+// logger.warn('This is a warning message');
+
+
+ import winston from 'winston';
+ import dotenv from "dotenv";
+ dotenv.config();
+ 
+ const logger = winston.createLogger({
+   level: 'info',
+   format: winston.format.combine(
+     winston.format.timestamp(),
+     winston.format.simple()
+   ),
+   transports: [
+     new winston.transports.Console(),
+     new winston.transports.File({ filename: process.env.LOGPATH??'./log/app.log'})
+ 
   ]
 });
 
-// Log messages
-logger.info('This is an information message');
-logger.error('This is an error message');
-logger.warn('This is a warning message');
-
 export default logger;
+
 
