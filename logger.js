1ca2af6d9a5f70dcf607813844f95b3bac6ca30c
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-03-20 15:05:56 -0400

    Added ops agent

--- logger.js
+++ logger.js
@@ -1,20 +1,3 @@
-// import winston from 'winston';
-
-// // Create a logger instance
-// const logger = winston.createLogger({
-//   level: 'info',
-//   format: winston.format.json(),
-//   transports: [
-//     new winston.transports.Console(),
-//     new winston.transports.File({ filename: '/var/log/webapp/myapp.log' })
-//   ]
-// });
-
-// // Log messages
-// logger.info('This is an information message');
-// logger.error('This is an error message');
-// logger.warn('This is a warning message');
-
 
 import winston from 'winston';
 import dotenv from "dotenv";
