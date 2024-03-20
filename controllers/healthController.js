c4982cc55dc2d62303c3ea8a8faa85c2da813f26
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-03-20 11:40:01 -0400

    Added ops agent

--- controllers/healthController.js
+++ controllers/healthController.js
@@ -8,7 +8,7 @@ const dbConnection = async (req, res, next) => {
     next(); 
   } catch (error) {
     console.error('Database Connectivity Error:', error);
-    //logger.error('Database Connectivity Error:', error);
+    logger.error('Database Connectivity Error:', error);
     res
       .status(503)
       .header("Cache-Control", "no-cache, no-store, must-revalidate")
@@ -23,6 +23,7 @@ const healthCheck = async (req, res) => {
   try {
     if (req.method === 'GET') {
       //await Model.create({ status: 'OK' });
+      logger.info('Database Connection Successful');
       res
         .status(200)
         .header('Cache-Control', 'no-cache, no-store, must-revalidate')
@@ -32,7 +33,7 @@ const healthCheck = async (req, res) => {
     }
   } catch (error) {
     console.error('Health Check Error:', error);
-    //logger.error('Health Check Error:', error);
+    logger.error('Health Check Error:', error);
     res
         .status(500)
         .header('Cache-Control', 'no-cache, no-store, must-revalidate')
