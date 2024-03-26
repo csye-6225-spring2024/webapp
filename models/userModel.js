111a0d3923b4a17df5df689391d28b50bcc940d8
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-03-26 16:20:44 -0400

    Added code to authenicate the user

--- models/userModel.js
+++ models/userModel.js
@@ -39,21 +39,21 @@ const User = sequelize.define('User', {
       allowNull: true,  
       readOnly: true,
     },
-    // is_verified: {
-    //   type: DataTypes.BOOLEAN,
-    //   defaultValue: false,
-    //   allowNull: false,
-    // },
-    // token: {
-    //   type: DataTypes.STRING,
-    //   allowNull: true,
-    // },
-    // validity: {
-    //   type: DataTypes.DATE,
-    //   //defaultValue: DataTypes,
-    //   allowNull: true,  
-    //   readOnly: true,
-    // }
+    is_verified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    validity: {
+      type: DataTypes.DATE,
+      //defaultValue: DataTypes,
+      allowNull: true,  
+      readOnly: true,
+    }
   },{
     timestamps: false,
   });
